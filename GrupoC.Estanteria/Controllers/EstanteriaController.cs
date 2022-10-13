9f87b54d47e0a6b89a68bd054eb8b59687e15575
victor9889 <victormusik98@hotmail.com>
2022-10-13 12:45:35 +0200

    Add EStanteria

---
+++ GrupoC.Estanteria/Controllers/EstanteriaController.cs
@@ -0,0 +1,24 @@
+﻿using GrupoC.Estanteria.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrupoC.Estanteria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstanteriaController : ControllerBase
+    {
+        private readonly IEstanteriaProvider estanteriaProvider;
+        public EstanteriaController(IEstanteriaProvider estanteriaProvider)
+        {
+            this.estanteriaProvider = estanteriaProvider;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(string id)
+        {
+            var result = estanteriaProvider.GetAsnyc(id);
+            if (result != null) return Ok(result);
+            return NotFound();
+        }
+    }
+}
